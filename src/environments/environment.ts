26d76048977d763e93c4bef801900f7b201e903b
Javier Brito <vjbritoa@hotmail.com>
2023-12-20 08:03:28 -0500

    Ajustes en producto y transaccion modulo VENTA v1.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   //url_seguridad: 'http://localhost:8080/asedinfo-servicios',
   url_seguridad: 'https://spring.asedinfo.com/asedinfo-servicios',
   //url_externo: 'http://localhost:5000',
-  url_externo: 'https://wp.asedinfo.com',
+  url_externo: 'https://whapp.asedinfo.com',
   //url_externo: 'http://51.79.53.164:5000',
 };
 
